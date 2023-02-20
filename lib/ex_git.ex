62ef3b80aee9c5d293ba559ca36cb919ab0893ea
Ed Bond <celpa.firl@gmail.com>
2023-02-20 15:36:53 -0600

    updating for docs

--- lib/ex_git.ex
+++ lib/ex_git.ex
@@ -1,6 +1,38 @@
 defmodule ExGit do
   @moduledoc """
   The shim between elixir and rust that gets overloaded
+
+
+  You can see more examples in the tests but here is a quick example:
+
+  ```
+    clone_prefix = "/tmp/example_git"
+    # init bare repo
+    {:ok, _} = ExGit.init_bare(clone_prefix <> "init_bare")
+
+    # clone to another directory
+    assert {:ok, "clone success"} ==
+             ExGit.clone(clone_prefix <> "init_bare", clone_prefix <> "push_a")
+
+    # clone another copy
+    assert {:ok, "clone success"} ==
+             ExGit.clone(clone_prefix <> "init_bare", clone_prefix <> "push_b")
+
+    # commit to a
+    :ok = File.write(clone_prefix <> "push_a/test.txt", "123")
+    ExGit.add_commit(clone_prefix <> "push_a", "push 123")
+
+    # push back changes so b can see it
+    assert {:ok, "Pushed Successfully"} == ExGit.push_remote(clone_prefix <> "push_a", "master")
+
+    # pull to check the file from b
+    assert {:ok, "Updated"} ==
+             ExGit.fast_forward(clone_prefix <> "push_b", "master")
+
+    assert {:ok, "Up to date already"} ==
+             ExGit.fast_forward(clone_prefix <> "push_b", "master")
+
+  ```
   """
   use Rustler, otp_app: :ex_git, crate: "exgit"
 
--- mix.exs
+++ mix.exs
@@ -4,10 +4,27 @@ defmodule ExGit.MixProject do
   def project do
     [
       app: :ex_git,
+      description: """
+      a lightweight rust shim for gitrs
+      """,
+      package: package(),
+      licenses: ["MIT"],
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: [
+        # The main page in the docs
+        main: "ExGit"
+      ]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Ed Bond"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/spunkedy/ExGit"}
     ]
   end
 
@@ -21,7 +38,8 @@ defmodule ExGit.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.27.0"}
+      {:rustler, "~> 0.27.0"},
+      {:ex_doc, "~> 0.29.1", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
